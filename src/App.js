5a0ce22ddc384f863afdb86b85c04bf9523679e9
User <user@MacBook-Pro-Mirage.local>
2022-12-07 17:08:45 +0200

    animated page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import {createRef} from "react";
 import {
     Routes,
     Route,
+    useLocation
 } from "react-router-dom";
+import {useTransition, animated} from 'react-spring';
 
 import './fonts/stylesheet.scss';
 
@@ -19,6 +21,18 @@ import HeadLogo from "./components/HeadLogo/HeadLogo";
 
 function App() {
     const header = createRef();
+    const location = useLocation();
+    const transition = useTransition(location, {
+        from: {
+            opacity: 0,
+        },
+        enter: {
+            opacity: 1
+        },
+        leave: {
+            opacity: 0
+        }
+    });
 
     const routeList = [
         {
@@ -29,32 +43,39 @@ function App() {
         ...questionText
     ]
 
-    function HandlerMouseMove (e) {
+    function HandlerMouseMove(e) {
         if (window.innerWidth > 768) {
-        let x = e.clientX / window.innerWidth;
-        let y = e.clientY / window.innerHeight;
+            let x = e.clientX / window.innerWidth;
+            let y = e.clientY / window.innerHeight;
 
-        let h1 = header.current;
-        h1.style.transform = 'translate(' + x * 50 + 'px, -' + y * 50 + 'px)';
+            let h1 = header.current;
+            h1.style.transform = 'translate(' + x * 50 + 'px, -' + y * 50 + 'px)';
         }
     }
 
 
-  return (
-    <div className="App" onMouseMove={e => HandlerMouseMove(e)}>
-        {/*what*/}
-        <Popup/>
-        <HeadLogo/>
-        <Lang/>
-        <WheelShow/>
-        <Ticker/>
-        <Circle/>
-        <Routes>
-            {routeList.map(item => <Route exact key={item.id} path={item.href} element={<MainTitle ref={header} title={item.title} />} />)}
-        </Routes>
-        {questionText.map(item => <Question title={item.title} href={item.href} key={item.id} className={item.className}/>)}
-    </div>
-  );
+    return (
+        <div className="App" onMouseMove={e => HandlerMouseMove(e)}>
+            <Popup/>
+            <HeadLogo/>
+            <Lang/>
+            <WheelShow/>
+            <Ticker/>
+            <Circle/>
+            {transition((props, item) => (
+                <animated.div style={props}>
+                    <Routes location={item.pathname}>
+                        {routeList.map(item => <Route exact key={item.id} path={item.href}
+                                                      element={<MainTitle ref={header} title={item.title}/>}/>)}
+                    </Routes>
+                </animated.div>
+            ))
+            }
+            {/*{routeList.map(item => <Route exact key={item.id} path={item.href} element={<MainTitle ref={header} title={item.title} />} />)}*/}
+            {questionText.map(item => <Question title={item.title} href={item.href} key={item.id}
+                                                className={item.className}/>)}
+        </div>
+    );
 }
 
 export default App;
